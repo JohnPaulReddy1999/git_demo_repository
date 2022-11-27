ea529198266e10cbd6f01dd64c4199a3f2f61819
JohnPaulReddy1999 <gopidijohnpaul@gmail.com>
2022-11-27 15:59:35 +0000

    changed in branch

--- git_demo_python.py
+++ git_demo_python.py
@@ -1,3 +1,13 @@
 print('hello')
 print('hi')
 print('hi hello')
+
+a= eval(input(("Enter a number")))
+list1=[2000,500,200,100,50,20,10,5,2,1]
+list2=[a]
+print("The amount requested",list2[-1])
+for i in range(0,len(list1)):
+    a=list2[-1]//list1[i]
+    print("No of",list1[i],"s ",a)
+    a=list2[-1]%list1[i]
+    list2.append(a)
