c21420fbe91cc1af9829a7bbe71896a6ef84c6b1
JohnPaulReddy1999 <gopidijohnpaul@gmail.com>
2022-11-27 15:56:55 +0000

    new commit

--- git_demo_python.py
+++ git_demo_python.py
@@ -1,3 +1,16 @@
 print('hello')
 print('hi')
 print('hi hello')
+
+passkey=4563
+for i in range(1,4):
+    Pin=eval(input("Enter A 4-Digit PIN"))
+    if(Pin==passkey):
+         print("Successfully Logined")
+    else:
+        for j in range(3):
+            if(Pin!=passkey and (j!=3 and print("Trials exuasted"))):
+                print("Invalid Pin")
+            
+                print("Ran out of trials")
+                break
